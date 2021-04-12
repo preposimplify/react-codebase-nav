ffef2a08d86d8f5bda59a63114613b87f8bfd005
kishankr7979 <41482800+kishankr7979@users.noreply.github.com>
2021-04-12 15:19:31 +0530

    Home-Content-Added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Nav from './Components/Nav';
 import About from './Components/about';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './Components/home';
 function App() {
   return (
     <Router>
     <div className="App">
       <Nav />
+      <Route path="/home" component={Home} />
       <Route path = "/about" component={About} />
     </div>
     </Router>
---
+++ src/Components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './main.css';
+
+const Main = () => {
+  return (
+    <h1>Hi, this is the content part</h1>
+  );
+}
+export default Main;
