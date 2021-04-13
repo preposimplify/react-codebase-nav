c50f7a60ee5559259036211587704b6d00ef4284
kishankr7979 <41482800+kishankr7979@users.noreply.github.com>
2021-04-14 02:46:55 +0530

    IT page Update

--- src/Components/IT.js
+++ src/Components/IT.js
@@ -1,9 +1,63 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Link, HashRouter,Switch } from "react-router-dom";
+import './it.css';
 const IT = () => {
   return (
     <div className="it-field">
-    <h1> Hi, This is IT Field </h1>
+    <h1>IT Companies</h1>
+    <hr />
+    <div className="Prod">
+    <h3>Product Based Companies</h3>
+  <span>*Companies Like Adobe, Amazon, Zscaler, Commvault, WorkIndia, Avalara and many more</span>
+  </div>
+  <br />
+    <div class="container">
+  <div className="row">
+    <div className="col-sm-6">
+      <b>DSA and Problem Solving</b>
+      <ul>
+      <li>Maths</li>
+      <li>Arrays</li>
+      <li>String</li>
+      <li>Matrix</li>
+      <li>Linked-list</li>
+      <li>Stack</li>
+      <li>Queue</li>
+      <li>Hashing</li>
+      <li>Tree</li>
+      <li>BST</li>
+      <li>Graph</li>
+      <li>Dynammic Programming</li>
+      <li>Trie</li>
+      <li>Bit Magic</li>
+
+      </ul>
+    </div>
+    <div className="col-sm">
+      <b>Subjective</b>
+      <ul type="none">
+      <li>OS</li>
+      <li>DBMS</li>
+      <li>System Design</li>
+      <li>Computer Networks</li>
+      <li>OOPs</li>
+
+
+      </ul>
+    </div>
+    <div className="col-sm">
+      <b>Development</b>
+      <ul type="none">
+      <li>Web</li>
+      <li>Mobile</li>
+      <li>ML</li>
+      <li>BlockChain</li>
+      <li>Cryptography</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
