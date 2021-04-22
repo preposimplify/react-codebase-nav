8bf55b578535a2dc4ceb39bc20403cb3ebddf569
kishankr7979 <41482800+kishankr7979@users.noreply.github.com>
2021-04-23 01:54:01 +0530

    Cards has been added on IT Page

--- src/Components/IT.js
+++ src/Components/IT.js
@@ -1,64 +1,64 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, HashRouter,Switch } from "react-router-dom";
 import './it.css';
-const IT = () => {
+import {FlippingCard, FlippingCardFront, FlippingCardBack} from 'react-ui-cards';
+import Product from './product';
+const IT = (props) => {
+  const styles1 = {
+    backgroundColor:"white",
+    paddingTop:'8em',
+
+  }
   return (
-    <div className="it-field">
-    <h1>IT Companies</h1>
-    <hr />
-    <div className="Prod">
-    <h3>Product Based Companies</h3>
-  <span>*Companies Like Adobe, Amazon, Zscaler, Commvault, WorkIndia, Avalara and many more</span>
-  </div>
-  <br />
-    <div class="container">
-  <div className="row">
-    <div className="col-sm-6">
-      <b>DSA and Problem Solving</b>
-      <ul>
-      <li>Maths</li>
-      <li>Arrays</li>
-      <li>String</li>
-      <li>Matrix</li>
-      <li>Linked-list</li>
-      <li>Stack</li>
-      <li>Queue</li>
-      <li>Hashing</li>
-      <li>Tree</li>
-      <li>BST</li>
-      <li>Graph</li>
-      <li>Dynammic Programming</li>
-      <li>Trie</li>
-      <li>Bit Magic</li>
-
-      </ul>
+   <div>
+   <BrowserRouter>
+   <div class="container h-100">
+  <div class="row align-middle">
+    <div class="col-md-6 col-lg-4 column">
+      <div class="card gr-1">
+        <div class="txt">
+          <h1>PRODUCT BASED<br />
+COMPANIES</h1>
+          <p>Companies like Adobe, Amazon, Commvault, Goldman Sachs, Lenovo, Zscaler</p>
+        </div>
+        <a href="#">more</a>
+        <div class="ico-card">
+        <i class="fa fa-rebel"></i>
+      </div>
+      </div>
     </div>
-    <div className="col-sm">
-      <b>Subjective</b>
-      <ul type="none">
-      <li>OS</li>
-      <li>DBMS</li>
-      <li>System Design</li>
-      <li>Computer Networks</li>
-      <li>OOPs</li>
-
-
-      </ul>
+    <div class="col-md-6 col-lg-4 column">
+      <div class="card gr-2">
+        <div class="txt">
+          <h1>FUNDED  <br />
+STARTUPS</h1>
+          <p>Companies like Navigus, Postman, Clicklabs, Bobble AI, Avalara Systems</p>
+        </div>
+        <a href="#">more</a>
+      <div class="ico-card">
+        <i class="fa fa-codepen"></i>
+      </div>
+      </div>
     </div>
-    <div className="col-sm">
-      <b>Development</b>
-      <ul type="none">
-      <li>Web</li>
-      <li>Mobile</li>
-      <li>ML</li>
-      <li>BlockChain</li>
-      <li>Cryptography</li>
-      </ul>
+    <div class="col-md-6 col-lg-4 column">
+      <div class="card gr-3">
+        <div class="txt">
+          <h1>SERVICE BASED <br />COMPANIES</h1>
+          <p>Companies like TCS, Infosys, Capgemini, Cognizant, Wipro, Accenture, Deloitte</p>
+        </div>
+        <Link to="#">click</Link>
+      <div class="ico-card">
+        <i class="fa fa-empire"></i>
+      </div>
+      </div>
     </div>
+    
   </div>
 </div>
+</BrowserRouter>
 
-    </div>
+</div>
+  
   );
 }
 export default IT;
