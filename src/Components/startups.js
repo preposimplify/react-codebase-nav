26021c0ee92e4d6dc5e252979ac2dd6bc5f7f721
kishankr7979 <41482800+kishankr7979@users.noreply.github.com>
2021-05-08 03:04:13 +0530

    Link has been updated on Cards

---
+++ src/Components/startups.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Startups = () => {
+	return (
+
+		<div className="sec1">
+		<h1>Welcome to Funded Startups based companies!</h1>
+		</div>
+	);
+}
+export default Startups;
